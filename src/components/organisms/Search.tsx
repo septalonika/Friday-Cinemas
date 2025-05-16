1eceefd458944579bd963d7143ed8f2d6cfd9497
septalonika <andikabeatbox@gmail.com>
2025-05-16 11:37:24 +0700

    using 2x2 for the mobile view

--- src/components/organisms/Search.tsx
+++ src/components/organisms/Search.tsx
@@ -50,7 +50,7 @@ const Search: React.FC = () => {
         <SearchNoResult Keywords={query} />
       ) : (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {movies.map((movie) => (
               <MovieCard key={movie.id} MovieDetails={movie} />
             ))}
