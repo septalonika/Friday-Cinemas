124d7deaf0dae28b99ffca6557c9edb7efce8403
septalonika <andikabeatbox@gmail.com>
2025-05-19 14:44:32 +0700

    update: using additional link to fetch MoviesByCategory

--- src/components/atoms/Header/Logo.tsx
+++ src/components/atoms/Header/Logo.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useMovieStore } from "../../../stores/movieStore";
 
 export const Logo: React.FC = () => {
+  const { fetchMoviesByCategory } = useMovieStore();
   return (
-    <Link to="/" className="flex items-center">
+    <Link to="/" className="flex items-center" onClick={() => fetchMoviesByCategory("popular", 1)}>
       <h1 className="sm:text-sm md:text-md lg:text-xl font-bold text-blue-500 ">
         Friday<span className="text-red-600 underline">Cine</span>
       </h1>
